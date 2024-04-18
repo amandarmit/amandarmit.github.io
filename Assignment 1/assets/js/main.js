89d26304baf64387f2f249bc988793b597286374
amandarmit <s3962616@student.rmit.edu.au>
2024-04-19 01:26:40 +1000

    changes made

--- Assignment 1/assets/js/main.js
+++ Assignment 1/assets/js/main.js
@@ -1,38 +1,9 @@
-(function($) {
+(function ($) {
   "use strict";
-  
- // menu 
-  $('.siteBar-btn').click( function (){ 
-    $('.mobile-menu').toggleClass('siteBar');
-    $('.menuShadow').toggleClass('show');
-  }); 
 
-    
-  // owlCarousel
-  $(".active_now_a_slider").owlCarousel({
-    loop: true,
-    margin: 30,
-    items: 6,
-    navText: [
-      '<i class="fa fa-angle-left"></i>',
-      '<i class="fa fa-angle-right"></i>'
-    ],
-    nav: false,
-    dots: false,
-    responsive: {
-      0: {
-        items: 2
-      },
-      767: {
-        items: 3
-      },
-      992: {
-        items: 6
-      }
-    }
+  // menu
+  $(".siteBar-btn").click(function () {
+    $(".mobile-menu").toggleClass("siteBar");
+    $(".menuShadow").toggleClass("show");
   });
-
- 
-  
- 
 })(jQuery);
